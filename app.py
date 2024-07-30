b1aa2d60d5b0eff7faa81205fb03cb6d581b2085
EswarSudhan <142371657+EswarSudhan@users.noreply.github.com>
2024-07-30 22:33:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
+x="innum illa"
 @app.route('/', methods=['GET'])
 def index():
-    return "success"
+    return x
 @app.route('/mapla', methods=['GET'])
 def mapla():
     return "Maja mapla"
+@app.route('/post', methods=['POST'])
+def maplapost():
+    x="post aiduchu"
+    return "post aiduchu"
 
 
 if __name__ == '__main__':
